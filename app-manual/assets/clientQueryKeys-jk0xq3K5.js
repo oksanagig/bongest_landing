f30f34df0160c45809f9cb90454da964b9e520fe
Oksana <oksana.bober@gmail.com>
2026-05-06 14:27:07 +0200

    Deploy gated beta app manual

---
+++ app-manual/assets/clientQueryKeys-jk0xq3K5.js
@@ -0,0 +1,1 @@
+const l={all:["clients"],fullList:()=>[...l.all,"full-list"],lookup:()=>[...l.all,"lookup"],taskEditable:()=>[...l.all,"task-editable"]};export{l as c};
---
+++ app-manual/assets/passwordPolicy-DC2Y4RCL.js
@@ -0,0 +1,1 @@
+const _="Au moins 10 caracteres";function e(N){return N.length<10?"Le mot de passe doit contenir au moins 10 caracteres":null}export{_ as A,e as v};
